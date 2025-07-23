db7a4bfdf727f40efcfa35ddf122592b033ef824
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-23 13:20:03 +0200

    toggle menu added on hamburguer icon

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrl: './navbar.css',
+  standalone: true,
 })
 export class Navbar {
+  isMenuOpen: boolean = false;
 
+  toggleMenu(): void{
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
