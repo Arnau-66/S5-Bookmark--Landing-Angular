bcb259ea5c96616916e11e4fe190e09066ceb912
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-24 12:51:32 +0200

    hero html linked to main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar],
+  imports: [Navbar, Hero],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'sprint5-angular';
 }
+
