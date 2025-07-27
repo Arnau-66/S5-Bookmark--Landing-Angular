f6772390e19fababdbfdd78d821a2c27b29e6b8d
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-27 20:16:04 +0200

    features-title component imported

--- src/app/shared/molecules/features-title/features-title.ts
+++ src/app/shared/molecules/features-title/features-title.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-features-title',
+  standalone: true,
   imports: [],
   templateUrl: './features-title.html',
   styleUrl: './features-title.css'
 })
 export class FeaturesTitle {
-
+  @Input() title: string = '';
+  @Input() text: string = '';
 }
--- src/app/shared/organisms/features/features.ts
+++ src/app/shared/organisms/features/features.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TabButton } from '../../atoms/tab-button/tab-button';
 import { Button } from '../../atoms/button/button';
+import { FeaturesTitle } from '../../molecules/features-title/features-title';
 
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [ TabButton, Button],
+  imports: [ TabButton, Button, FeaturesTitle],
   templateUrl: './features.html',
   styleUrl: './features.css'
 })
