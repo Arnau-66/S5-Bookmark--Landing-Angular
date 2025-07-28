4853abe8089d43765536a5f8cc8f5834c8ee4daa
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-28 19:21:47 +0200

    button atom component added on questions section

--- src/app/shared/molecules/question-item/question-item.ts
+++ src/app/shared/molecules/question-item/question-item.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-question-item',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './question-item.css'
 })
 export class QuestionItem {
-
+  @Input() question: string = '';
 }
--- src/app/shared/organisms/questions/questions.ts
+++ src/app/shared/organisms/questions/questions.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FeaturesTitle } from '../../molecules/features-title/features-title';
+import { QuestionItem } from '../../molecules/question-item/question-item';
+import { Button } from '../../atoms/button/button';
 
 @Component({
   selector: 'app-questions',
   standalone: true,
-  imports: [FeaturesTitle],
+  imports: [FeaturesTitle, QuestionItem, Button],
   templateUrl: './questions.html',
   styleUrl: './questions.css'
 })
