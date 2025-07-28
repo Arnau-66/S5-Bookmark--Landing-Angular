454ddc08cce7683c865158febb65646547c12eb4
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-28 17:55:59 +0200

    extension-card componen exported to extension component

--- src/app/shared/organisms/extension/extension.ts
+++ src/app/shared/organisms/extension/extension.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FeaturesTitle } from '../../molecules/features-title/features-title';
+import { ExtensionCard } from '../../molecules/extension-card/extension-card';
 
 @Component({
   selector: 'app-extension',
   standalone: true,
-  imports: [FeaturesTitle],
+  imports: [FeaturesTitle, ExtensionCard],
   templateUrl: './extension.html',
   styleUrl: './extension.css'
 })
