c8d36648ddfac749ad02dfe7ed2cb6565339a726
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-28 13:11:29 +0200

    features button view problems fixed

--- src/app/shared/molecules/features-panel/features-panel.ts
+++ src/app/shared/molecules/features-panel/features-panel.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Button } from '../../atoms/button/button';
 
 @Component({
   selector: 'app-features-panel',
-  imports: [],
+  standalone: true,
+  imports: [Button],
   templateUrl: './features-panel.html',
   styleUrl: './features-panel.css'
 })
 export class FeaturesPanel {
+@Input() backgroundImage: string = '';
+@Input() illustration: string = '';
+@Input() title: string = '';
+@Input() description: string = '';
+@Input() buttonLabel: string = 'More info';
 
 }
--- src/app/shared/organisms/features/features.ts
+++ src/app/shared/organisms/features/features.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TabButton } from '../../atoms/tab-button/tab-button';
 import { Button } from '../../atoms/button/button';
 import { FeaturesTitle } from '../../molecules/features-title/features-title';
+import { FeaturesPanel } from '../../molecules/features-panel/features-panel';
 
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [ TabButton, Button, FeaturesTitle],
+  imports: [ TabButton, Button, FeaturesTitle, FeaturesPanel],
   templateUrl: './features.html',
   styleUrl: './features.css'
 })
