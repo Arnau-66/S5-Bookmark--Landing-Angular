4fe0e3e1a07eae00647dd57b1d34ea310cd06035
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-29 09:43:01 +0200

    answer added to question-item component

--- src/app/shared/molecules/question-item/question-item.ts
+++ src/app/shared/molecules/question-item/question-item.ts
@@ -2,10 +2,18 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-question-item',
+  standalone: true,
   imports: [],
   templateUrl: './question-item.html',
   styleUrl: './question-item.css'
 })
 export class QuestionItem {
   @Input() question: string = '';
+  @Input() answer: string = '';
+
+  isOpen: boolean = false;
+
+    toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
