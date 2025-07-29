50b6752ba3d4108f8cf11c450d5f3a2e57fbf6b7
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-29 11:25:02 +0200

    newsletter component created (newsletter molecule imported)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { Hero } from './shared/organisms/hero/hero';
 import { Features } from './shared/organisms/features/features';
 import { Extension } from './shared/organisms/extension/extension';
 import { Questions } from './shared/organisms/questions/questions';
+import { Newsletter } from './shared/organisms/newsletter/newsletter';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar, Hero, Features, Extension, Questions],
+  imports: [Navbar, Hero, Features, Extension, Questions, Newsletter],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/shared/organisms/newsletter/newsletter.ts
+++ src/app/shared/organisms/newsletter/newsletter.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NewsletterForm } from '../../molecules/newsletter-form/newsletter-form';
 
 @Component({
   selector: 'app-newsletter',
-  imports: [],
+  standalone: true,
+  imports: [NewsletterForm],
   templateUrl: './newsletter.html',
   styleUrl: './newsletter.css'
 })
