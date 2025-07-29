6c166b5e25c46df712d99ec4c5e39083fae18f2a
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-29 12:45:31 +0200

    blur validation on input-field component

--- src/app/shared/atoms/input-field/input-field.ts
+++ src/app/shared/atoms/input-field/input-field.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './input-field.html',
   styleUrl: './input-field.css'
 })
 export class InputField {
   @Input() placeholder: string = ``;
+
+  value: string ='';
+  error: string ='';
+
+  validate(): void {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailRegex.test(this.value)) {
+      this.error = 'Please enter a valid email address.';
+
+    } else {
+      this.error = '';
+    }
+  }
 }
--- src/app/shared/molecules/newsletter-form/newsletter-form.ts
+++ src/app/shared/molecules/newsletter-form/newsletter-form.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { InputField } from '../../atoms/input-field/input-field';
 import { Button } from '../../atoms/button/button';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-newsletter-form',
-  imports: [InputField, Button],
+  standalone: true,
+  imports: [InputField, Button, FormsModule],
   templateUrl: './newsletter-form.html',
   styleUrl: './newsletter-form.css'
 })
