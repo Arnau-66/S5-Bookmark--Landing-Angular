728c46eb85cc992e7fe46f2e8d377e39dc873c94
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-29 09:27:11 +0200

    Button input deleted from Features component

--- src/app/shared/organisms/features/features.ts
+++ src/app/shared/organisms/features/features.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { TabButton } from '../../atoms/tab-button/tab-button';
-import { Button } from '../../atoms/button/button';
 import { FeaturesTitle } from '../../molecules/features-title/features-title';
 import { FeaturesPanel } from '../../molecules/features-panel/features-panel';
 
@@ -9,7 +8,7 @@ type TabKey = 'Simple Bookmarking' | 'Speedy Searching' | 'Easy Sharing';
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [ TabButton, Button, FeaturesTitle, FeaturesPanel],
+  imports: [ TabButton, FeaturesTitle, FeaturesPanel],
   templateUrl: './features.html',
   styleUrl: './features.css'
 })
