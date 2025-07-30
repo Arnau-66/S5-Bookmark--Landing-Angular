299b8bf5d021f9d440aea054e78c8454c43df660
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-30 09:31:49 +0200

    footer section merged to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { Features } from './shared/organisms/features/features';
 import { Extension } from './shared/organisms/extension/extension';
 import { Questions } from './shared/organisms/questions/questions';
 import { Newsletter } from './shared/organisms/newsletter/newsletter';
+import { Footer } from './shared/organisms/footer/footer';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar, Hero, Features, Extension, Questions, Newsletter],
+  imports: [Navbar, Hero, Features, Extension, Questions, Newsletter, Footer],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
