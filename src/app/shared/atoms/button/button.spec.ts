ddf032f7362b2c6ec5dec1f6ad95b6f926f4cd4a
Arnau Perez <arnauperezesteve@gmail.com>
2025-08-08 15:25:01 +0200

    Added new tests to button component (render, href & variant class)

--- src/app/shared/atoms/button/button.spec.ts
+++ src/app/shared/atoms/button/button.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Button } from './button';
+import { By } from '@angular/platform-browser';
+
 
 describe('Button', () => {
   let component: Button;
@@ -20,4 +21,28 @@ describe('Button', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the input text', () => {
+    component.text = 'CLICK ME';
+    fixture.detectChanges();
+    const link: HTMLAnchorElement = fixture.nativeElement.querySelector('a');
+    expect(link.textContent?.trim()).toBe('CLICK ME');
+  });
+
+  it('should set the correct href', () => {
+    component.href = '/test-link';
+    fixture.detectChanges();
+    const link: HTMLAnchorElement = fixture.nativeElement.querySelector('a');
+    expect(link.getAttribute('href')).toBe('/test-link');
+  });
+
+  it('should apply the variant class', () => {
+    component.variant = 'firefox';
+    fixture.detectChanges();
+    const linkDe = fixture.debugElement.query(By.css('a'));
+    expect(linkDe.classes['firefox']).toBeTrue();
+  });
+
+
+
 });
