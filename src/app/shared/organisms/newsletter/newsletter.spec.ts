8ca9c6d9205e033aa170a6f20d163e1e7e43ea6d
Arnau Perez <arnauperezesteve@gmail.com>
2025-08-10 11:21:51 +0200

    Added test to navbar component (render newsletter section container & render NewsletterForm child component)

--- src/app/shared/organisms/newsletter/newsletter.spec.ts
+++ src/app/shared/organisms/newsletter/newsletter.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Newsletter } from './newsletter';
+import { By } from '@angular/platform-browser';
+import { NewsletterForm } from '../../molecules/newsletter-form/newsletter-form';
+
 
 describe('Newsletter', () => {
   let component: Newsletter;
@@ -20,4 +22,18 @@ describe('Newsletter', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the newsletter section container', () => {
+    const section: HTMLElement | null = fixture.nativeElement.querySelector('.newsletter');
+    expect(section).not.toBeNull();
+  });
+
+  it('should render a NewsletterForm child component', () => {
+    const formDe = fixture.debugElement.query(By.directive(NewsletterForm));
+    expect(formDe).toBeTruthy();
+   
+    const allForms = fixture.debugElement.queryAll(By.directive(NewsletterForm));
+    expect(allForms.length).toBe(1);
+  });
+
 });
