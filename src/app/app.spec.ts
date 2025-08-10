cf60bf71cd2c07fa4b4fb1602685eaeb33a43a54
Arnau Perez <arnauperezesteve@gmail.com>
2025-08-10 11:31:02 +0200

    Added test to app.component (render top-level organisms & render one of each organisms)

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,5 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+
+import { Navbar } from './shared/organisms/navbar/navbar';
+import { Hero } from './shared/organisms/hero/hero';
+import { Features } from './shared/organisms/features/features';
+import { Extension } from './shared/organisms/extension/extension';
+import { Questions } from './shared/organisms/questions/questions';
+import { Newsletter } from './shared/organisms/newsletter/newsletter';
+import { Footer } from './shared/organisms/footer/footer';
+
 
 describe('App', () => {
   beforeEach(async () => {
@@ -20,4 +30,31 @@ describe('App', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('A Simple Bookmark Manager');
   });
+
+  it('should render all top-level organisms', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.directive(Navbar))).toBeTruthy();
+    expect(fixture.debugElement.query(By.directive(Hero))).toBeTruthy();
+    expect(fixture.debugElement.query(By.directive(Features))).toBeTruthy();
+    expect(fixture.debugElement.query(By.directive(Extension))).toBeTruthy();
+    expect(fixture.debugElement.query(By.directive(Questions))).toBeTruthy();
+    expect(fixture.debugElement.query(By.directive(Newsletter))).toBeTruthy();
+    expect(fixture.debugElement.query(By.directive(Footer))).toBeTruthy();
+  });
+
+  it('should render exactly one of each organism', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.directive(Navbar)).length).toBe(1);
+    expect(fixture.debugElement.queryAll(By.directive(Hero)).length).toBe(1);
+    expect(fixture.debugElement.queryAll(By.directive(Features)).length).toBe(1);
+    expect(fixture.debugElement.queryAll(By.directive(Extension)).length).toBe(1);
+    expect(fixture.debugElement.queryAll(By.directive(Questions)).length).toBe(1);
+    expect(fixture.debugElement.queryAll(By.directive(Newsletter)).length).toBe(1);
+    expect(fixture.debugElement.queryAll(By.directive(Footer)).length).toBe(1);
+  });
+
 });
